72475e4341500e7747b9f51d9c8f825ebbe6a8a3
guido <guidofranco-@hotmail.com>
2021-07-15 23:56:39 -0300

    data from urls

--- dag.py
+++ dag.py
@@ -11,25 +11,7 @@ import requests
 
 
 home_dir = os.environ["HOME"]
-
-
-def download_data():
-    """
-    Descargar los datos del dataset publico de IMDB
-    y guardarlos en el directorio HOME
-    """
-    url_base = "https://datasets.imdbws.com"
-    fnames = [
-        "name.basics.tsv.gz", "title.akas.tsv.gz", "title.basics.tsv.gz",
-        "title.crew.tsv.gz", "title.episode.tsv.gz",
-        "title.principals.tsv.gz", "title.ratings.tsv.gz"]
-
-    for fname in fnames:
-        url = os.path.join(url_base, fname)
-        resp = requests.get(url)
-
-        data_dir = os.path.join(home_dir, "data", fname)
-        open(data_dir, "wb").write(resp.content)
+url_base = "https://datasets.imdbws.com"
 
 
 def transform_movies_data():
@@ -37,9 +19,13 @@ def transform_movies_data():
     Obtener y transformar los datos de peliculas
     y guardar el resultado en el directorio stating
     """
-    titles_df = pd.read_csv(f"{home_dir}/data/title.basics.tsv.gz",
-                            sep="\t", encoding="utf-8",
-                            dtype="object", na_values=r"\N")
+    file_name = "title.basics.tsv.gz"
+    url_base = "https://datasets.imdbws.com"
+    url_file = f"{url_base}/{file_name}"
+    titles_df = pd.read_csv(
+                    url_file,
+                    sep="\t", encoding="utf-8",
+                    dtype="object", na_values=r"\N")
 
     # obtener solos los titulo de tipo "pelicula"
     movies_mask = titles_df["titleType"] == "movie"
@@ -67,8 +53,8 @@ def transform_movies_data():
 
     # Guardar el resultado en un archivo csv en el directorio staging
     movies_df.to_csv(
-        f"{home_dir}/staging/movies_basics.tsv",
-        index=None, sep="\t")
+        f"{home_dir}/staging/movies_basics.csv",
+        index=None, sep=";")
 
 
 def transform_crew_data():
@@ -76,17 +62,14 @@ def transform_crew_data():
     Leer y transformar los datos sobre directores y escritores
     y guardar el resultado en el directorio staging
     """
-    directors_df = pd.read_csv(
+    crew_df = pd.read_csv(
                         f"{home_dir}/data/title.crew.tsv.gz",
                         sep="\t", encoding="utf-8",
-                        usecols=["tconst", "directors"],
+                        usecols=["tconst", "directors", "writers"],
                         na_values=r"\N")
 
-    writers_df = pd.read_csv(
-                        f"{home_dir}/data/title.crew.tsv.gz", sep="\t",
-                        encoding="utf-8",
-                        usecols=["tconst", "writers"],
-                        na_values=r"\N")
+    directors_df = crew_df[["tconst", "directors"]]
+    writers_df = crew_df[["tconst", "writers"]]
 
     directors_df = directors_df.dropna(subset=["directors"])
     writers_df = writers_df.dropna(subset=["writers"])
@@ -101,11 +84,11 @@ def transform_crew_data():
 
     # Guardar ambos dataframes en un archivo csv en el directorio staging
     directors_df.to_csv(
-                    f"{home_dir}/staging/directors.tsv",
-                    sep="\t", index=None)
+                    f"{home_dir}/staging/directors.csv",
+                    sep=";", index=None)
     writers_df.to_csv(
-                    f"{home_dir}/staging/writers.tsv",
-                    sep="\t", index=None)
+                    f"{home_dir}/staging/writers.csv",
+                    sep=";", index=None)
 
 
 def join_data():
@@ -116,16 +99,16 @@ def join_data():
     - rating promedio y cantidad de votos recibidos
     """
     directors_df = pd.read_csv(
-                        f"{home_dir}/staging/writers.tsv", sep="\t")
+                        f"{home_dir}/staging/writers.csv", sep=";")
 
     writers_df = pd.read_csv(
-                        f"{home_dir}/staging/directors.tsv", sep="\t")
+                        f"{home_dir}/staging/directors.csv", sep=";")
 
     basics_df = pd.read_csv(
-                        f"{home_dir}/staging/movies_basics.tsv", sep="\t")
+                        f"{home_dir}/staging/movies_basics.csv", sep=";")
 
     ratings_df = pd.read_csv(
-                        f"{home_dir}/data/title.ratings.tsv.gz",
+                        f"{url_base}/title.ratings.tsv.gz",
                         sep="\t", encoding="utf-8")
 
     joined_df = basics_df.join(
@@ -162,9 +145,10 @@ def aggregate_data(joined_df):
                         columns={
                             "directors": "numDirectors",
                             "writers": "numWriters"
-                            })
+                            }) \
+                    .round(2)
 
-    target_df.to_csv(f"{home_dir}/results/results.csv", sep="\t")
+    target_df.to_csv(f"{home_dir}/results.csv", sep=";")
 
 
 def join_and_agg_data():
@@ -184,22 +168,16 @@ with DAG(
         ) as dag:
 
     t1 = PythonOperator(
-        task_id="download_data",
-        python_callable=download_data)
-
-    t2 = PythonOperator(
         task_id="transform_basics_data",
         python_callable=transform_movies_data)
 
-    t3 = PythonOperator(
+    t2 = PythonOperator(
         task_id="transform_crew_data",
         python_callable=transform_crew_data)
 
-    t4 = PythonOperator(
+    t3 = PythonOperator(
         task_id="join_and_aggregate_data",
         python_callable=join_and_agg_data)
 
-t1 >> t2
 t1 >> t3
-t2 >> t4
-t3 >> t4
+t2 >> t3
